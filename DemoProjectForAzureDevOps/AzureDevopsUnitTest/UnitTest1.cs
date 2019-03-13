44b333cb95d64233861c811b47d01e1130cee211
Vaibhav Srivastava <vaibhavsrivastava95@gmail.com>
2019-03-13 16:21:49 +0530

    New BUild Test

--- DemoProjectForAzureDevOps/AzureDevopsUnitTest/UnitTest1.cs
+++ DemoProjectForAzureDevOps/AzureDevopsUnitTest/UnitTest1.cs
@@ -9,7 +9,7 @@ namespace AzureDevopsUnitTest
 		[TestMethod]
 		public void TestMethod1()
 		{
-			Console.WriteLine("I am here.fdfghghgh");
+			Console.WriteLine("I am here.");
 			//throw new Exception();
 		}
 	}
